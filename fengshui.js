9979abfdd20351e14686038d8323b73060db9c7e
Frontenddevkan <frontenddevkan@gmail.com>
2023-09-23 14:57:15 +0500

    feat: add js file fot heaven

---
+++ fengshui.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log('Hello! I am a Chinese Calendar');
+
+const arrHeaven = ["bin", "din", "wu", "dzi", "gen", "sin", "ren", "gui", "Dzja", "i"];
+
+const pathIeroglifs = [];
+
+fot(let i arrHeaven){
+
+}
