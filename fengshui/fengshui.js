8252e3eff0073839838f3c1dc81243741cc32408
Frontenddevkan <frontenddevkan@gmail.com>
2023-09-25 20:36:09 +0500

    feat: add fonts

---
+++ fengshui/fengshui.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// показываем один элемент из массива в строке.
+const arrayHeaven = ["i", "bin", "din", "wu", "dzi", "gen", "sin", "ren", "gui", "Dzja"];
+
+function getHeaven() {
+        let heavenVariable = arrayHeaven[0];
+        return document.getElementById('heaven').innerHTML = heavenVariable;
+}
+
+getHeaven();
+
+// теперь нужно связать дату и индекс массива.
+
+// const arrHeaven = ['i', 'bin', 'din', 'wu', 'dzi', 'gen', 'sin', 'ren', 'gui', 'Dzja'];
+
+date
+
+// const pathArrayHeaven = [];
+
+// for(let i in arrayHeaven){
+//   pathArrayIeroglif.push(`./img/${arrHeaven[i]}.svg`);
+// }
+
+// console.log(pathIeroglif);
+
+// // выводим получившиеся данные в хтмл
+
+// const data = [
+//     {date: '2023-09-24', pathIeroglif[0]},
+//     {date: '2023-09-25', pathIeroglif[1]},
+//     {date: '2023-09-26', pathIeroglif[2]},
+//     {date: '2023-09-27', pathIeroglif[3]},
+//     {date: '2023-09-28', pathIeroglif[4]},
+//     {date: '2023-09-29', pathIeroglif[5]},
+//     {date: '2023-09-30', pathIeroglif[6]},
+//     {date: '2023-10-01', pathIeroglif[7]},
+//     {date: '2023-10-02', pathIeroglif[8]},
+//     {date: '2023-10-03', pathIeroglif[9]}
+// ]
+// function displayDataByDate(date) {
+//     const container = document.getElementById('heaven');
+//     container.innerHTML = '';
+
+//     data.forEach(item => {
+//         if(item.date === date){
+//             const eventElement = document.createElement('div');
+//             eventElement.divContent = item.pathIeroglif[];
+//             container.appendChild(eventElement);
+
+//         }
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('переделывать?');
