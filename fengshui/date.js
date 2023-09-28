2141cb1001832d5e3ade5a5a7eb2b28b2de4b00f
Frontenddevkan <frontenddevkan@gmail.com>
2023-09-28 21:34:41 +0500

    fix: splice date from other algorythms

---
+++ fengshui/date.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const arrayHeaven = ["ren", "gui", "Dzja", "i", "bin", "din", "wu", "dzi", "gen", "sin"];
+const months = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь"
+  ];
+
+
+  let now = new Date();
+  let currentMonth = now.getMonth();
+  let monthWord = months[currentMonth];
+
+let currentYear = now.getFullYear();
+let currentDay = now.getDay();
+let currentHour = now.getHours();
+let currentMinute = now.getMinutes();
+let currentSecond = now.getSeconds();
+let current_time = currentHour + "." + currentMinute + "." + currentSecond;
+
+document.getElementById("hour-time").innerHTML = current_time;
+document.getElementById("day-time").innerHTML = currentDay;
+document.getElementById("month-time").innerHTML = monthWord;
+document.getElementById("year-time").innerHTML = currentYear;
+
+
+
