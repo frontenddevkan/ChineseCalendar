d91e659e4d5b281cbba2098204a0d28ac2844050
Frontenddevkan <frontenddevkan@gmail.com>
2023-10-02 01:51:59 +0500

    feat: add heavenly algorythms

--- fengshui/heavenlyTrunks.js
+++ fengshui/heavenlyTrunks.js
@@ -1,34 +1,25 @@
 'use strict';
 
 const arrayHeaven = ["ren", "gui", "Dzja", "i", "bin", "din", "wu", "dzi", "gen", "sin"];
+const pathArrayHeaven = [];
 
-
-for(let i = 0; i < 10; i++) {
-    arrayHeaven[i] = i + 1;
-
+for(let i = 0; i < arrayHeaven.length; i++) {
+    pathArrayHeaven.push(`../img/${arrayHeaven[i]}.svg`);
 }
-
+console.log(pathArrayHeaven);
 
 const currentDay = new Date();
 const currentHour = currentDay.getHours();
 const currentMinute = currentDay.getMinutes();
-
 const startPoint = new Date(2023, 9, 1, 23, 0); // 1 october 2023 23:00
-
 const differentTime = Math.floor((currentDay.getTime() - startPoint.getTime()) / 1000 * 60);
+const digitIndex = Math.floor((differentTime / 120) % 10); // 120 minutes in two hours. and 10 elements.
 
-const digitIndex = Math.floor(differentTime / 120) % 10; // 120 minutes in two hours. and 10 elements.
+let img = document.createElement('img');
+img.src = pathArrayHeaven[digitIndex];
 
-const digit = digitIndex + 1; // 1, 2, 3
-
-
-
-
-console.log('current index: ' + digit);
-
-
-
-
-
+const diveHeaven = document.getElementById('div-heaven');
+diveHeaven.innerHTML = '';
+diveHeaven.appendChild(img);
 
 
