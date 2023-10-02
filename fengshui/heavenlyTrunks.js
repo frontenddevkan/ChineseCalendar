33ddb38e8c23c36676ecaee9c965716d0b2bb51f
Frontenddevkan <frontenddevkan@gmail.com>
2023-10-03 01:31:47 +0500

    feat: add algorythms of month and years

--- fengshui/heavenlyTrunks.js
+++ fengshui/heavenlyTrunks.js
@@ -11,7 +11,7 @@ console.log(pathArrayHeaven);
 const startPoint = new Date(2023, 9, 1, 23, 0); // 1 october 2023 23:00
 
 
-const differentTimeHour = Math.floor(now.getTime() - startPoint.getTime()) / (1000 * 60 * 120);
+const differentTimeHour = Math.floor(now - startPoint.getTime()) / (1000 * 60 * 120);
 
 const digitIndexHour = Math.floor(differentTimeHour % 10); // 120 minutes in two hours. and 10 elements.
 console.log(digitIndexHour);
@@ -41,3 +41,50 @@ diveHeavenDay.innerHTML = '';
 diveHeavenDay.appendChild(imgDay);
 
 
+// for Month cycles
+const arrayHeavenMonth = ["sin", "ren", "gui", "Dzja", "i", "bin", "din", "wu", "dzi", "gen"];
+const pathArrayHeavenMonth = [];
+for(let ite = 0; ite < arrayHeavenMonth.length; ite++) {
+    pathArrayHeavenMonth.push(`../img/${arrayHeavenMonth[ite]}.svg`);
+}
+console.log(pathArrayHeaven);
+
+
+const startDateMonth = new Date('2023-09-01');
+const currentDateMonth = new Date();
+
+const differentMonth = Math.floor(currentDateMonth - startDateMonth) / (30 * 24 * 60 * 60 * 1000) ;
+console.log(differentMonth);
+const digitIndexMonth = Math.floor(differentMonth % 10); // month and 10 elements
+
+const imgMonth = document.createElement('img');
+imgMonth.src = pathArrayHeavenMonth[digitIndexMonth];
+
+const diveHeavenMonth = document.getElementById('div-heaven-month');
+diveHeavenMonth.innerHTML = '';
+diveHeavenMonth.appendChild(imgMonth);
+
+
+
+// for YEAR cycles
+const arrayHeavenYear = ["gui", "Dzja", "i", "bin", "din", "wu", "dzi", "gen", "sin", "ren"];
+const pathArrayHeavenYear = [];
+for(let item = 0; item < arrayHeavenYear.length; item++) {
+    pathArrayHeavenYear.push(`../img/${arrayHeavenYear[item]}.svg`);
+}
+console.log(pathArrayHeavenYear);
+
+
+const startDateYear = new Date('2023-01-04');
+const currentDateYear = new Date();
+
+const differentYear = Math.floor(currentDateYear - startDateYear) / (12 * 30 * 24 * 60 * 60 * 1000) ;
+
+const digitIndexYear = Math.floor(differentYear % 10); // year and 10 elements
+
+const imgYear = document.createElement('img');
+imgYear.src = pathArrayHeavenYear[digitIndexYear];
+
+const diveHeavenYear = document.getElementById('div-heaven-year');
+diveHeavenYear.innerHTML = '';
+diveHeavenYear.appendChild(imgYear);
