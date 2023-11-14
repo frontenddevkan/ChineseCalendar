02cf20c632eb67917f59b7ce26fe60e95a463b22
Frontenddevkan <frontenddevkan@gmail.com>
2023-11-14 18:52:12 +0500

    feat: add pics to earth

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,27 @@
 "use strict";
 
+
+
+
+// JavaScript used to set randomness for particles.
+// Could be done via SSR
+
+const RANDOM = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+const PARTICLES = document.querySelectorAll('.particle')
+
+PARTICLES.forEach(P => {
+	P.setAttribute('style', `
+		--x: ${RANDOM(20, 80)};
+		--y: ${RANDOM(20, 80)};
+		--duration: ${RANDOM(6, 20)};
+		--delay: ${RANDOM(1, 10)};
+		--alpha: ${RANDOM(40, 90) / 100};
+		--origin-x: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
+		--origin-y: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
+		--size: ${RANDOM(40, 90) / 100};
+	`)
+})
+
 //new Date
 //Благодаря вращению Земли вокруг своей оси - смена суток
 //Солнечные ритмы - солнечный китайский календарь
