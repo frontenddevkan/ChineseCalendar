516224f32079ed3604b68203d571bec8deddfe0b
Frontenddevkan <frontenddevkan@gmail.com>
2023-11-19 21:00:50 +0500

    fix: fix js

--- fengshui/heavenlyTrunks.js
+++ fengshui/heavenlyTrunks.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// for hours cycles time
+
+
 const arrayHeaven = ["ren", "gui", "Dzja", "i", "bin", "din", "wu", "dzi", "gen", "sin"];
 const pathArrayHeaven = [];
 
 for(let i = 0; i < arrayHeaven.length; i++) {
     pathArrayHeaven.push(`../img/${arrayHeaven[i]}.svg`);
 }
-console.log(pathArrayHeaven);
+
 
 const startPoint = new Date(2023, 9, 1, 23, 0); // 1 october 2023 23:00
 
@@ -14,7 +17,7 @@ const startPoint = new Date(2023, 9, 1, 23, 0); // 1 october 2023 23:00
 const differentTimeHour = Math.floor(now - startPoint.getTime()) / (1000 * 60 * 120);
 
 const digitIndexHour = Math.floor(differentTimeHour % 10); // 120 minutes in two hours. and 10 elements.
-console.log(digitIndexHour);
+
 
 const img = document.createElement('img');
 img.src = pathArrayHeaven[digitIndexHour];
