aa85d60b85d48ec083a79c8dae5ca66ab56dde2d
Frontenddevkan <frontenddevkan@gmail.com>
2023-11-20 18:33:13 +0500

    feat: add earth trunks

---
+++ fengshui/earthTrunks.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// for time for ouhrs earth trunks
+
+const arrayEarth = ["11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const pathArrayEarth = [];
+
+for(let i = 0; i < arrayEarth.length; i++) {
+    pathArrayEarth.push(`../img/${arrayEarth[i]}.svg`);
+}
+
+
+const startPointEarth = new Date(2023, 10, 14, 21, 0); // 14 november 2023 21:00
+
+const differentTimeHourEarth = Math.floor(now - startPointEarth.getTime()) / (1000 * 60 * 120);
+
+const digitIndexHourEarth = Math.floor(differentTimeHourEarth % 12); // 120 minutes in two hours. and 12 elements.
+
+const imgEarthHour = document.createElement('img');
+imgEarthHour.src = pathArrayEarth[digitIndexHourEarth];
+
+const diveEarthHour = document.getElementById('div-earth-hour');
+diveEarthHour.innerHTML = '';
+diveEarthHour.appendChild(imgEarthHour);
+
+// for Day cycles
+
+const startDateEarth = new Date('2023-10-20'); // 20 november 2023;
+
+const currentDateEarth = new Date();
+
+const differentTimeDayEarth = Math.floor(currentDateEarth - startDateEarth) / (24 * 60 * 60 * 1000) ;
+
+const digitIndexDayEarth = Math.floor(differentTimeDayEarth % 12); // days and 12 elements
+
+const imgDayEarth = document.createElement('img');
+imgDayEarth.src = pathArrayEarth[digitIndexDayEarth];
+
+const divEarthDay = document.getElementById('div-earth-day');
+divEarthDay.innerHTML = '';
+divEarthDay.appendChild(imgDayEarth);
+
+// for Month cycles
+
+const startDateEarthMonth = new Date('2023-11-20'); // 20 november 2023;
+
+const currentEarthDateMonth = new Date();
+
+const differentMonthEarth = Math.floor(currentEarthDateMonth - startDateEarthMonth) / (30 * 24 * 60 * 60 * 1000);
+
+const digitIndexEarthMonth = Math.floor(differentMonthEarth % 12); // month and 12 elements
+
+const imgMonthEarth = document.createElement('img');
+imgMonthEarth.src = pathArrayEarth[digitIndexEarthMonth];
+
+const divEarthMonth = document.getElementById('div-earth-month');
+divEarthMonth.innerHTML = '';
+
+divEarthMonth.appendChild(imgMonthEarth);
+
+// for YEAR cycles
+
+const startDateEarthYear = new Date('2019-1-1'); // 20 november 2023;
+const currentEarthDateYear = new Date();
+
+const differentYearEarth = Math.floor(currentEarthDateYear - startDateEarthYear) / (365 * 24 * 60 * 60 * 1000);
+
+
+const digitIndexEarthYear = Math.floor(differentYearEarth % 12); // month and 12 elements
+
+const imgYearEarth = document.createElement('img');
+imgYearEarth.src = pathArrayEarth[digitIndexEarthYear];
+
+const divEarthYear = document.getElementById('div-earth-year');
+divEarthYear.innerHTML = '';
+divEarthYear.appendChild(imgYearEarth);
+
+
+
--- fengshui/heavenlyTrunks.js
+++ fengshui/heavenlyTrunks.js
@@ -13,7 +13,6 @@ for(let i = 0; i < arrayHeaven.length; i++) {
 
 const startPoint = new Date(2023, 9, 1, 23, 0); // 1 october 2023 23:00
 
-
 const differentTimeHour = Math.floor(now - startPoint.getTime()) / (1000 * 60 * 120);
 
 const digitIndexHour = Math.floor(differentTimeHour % 10); // 120 minutes in two hours. and 10 elements.
@@ -22,9 +21,9 @@ const digitIndexHour = Math.floor(differentTimeHour % 10); // 120 minutes in two
 const img = document.createElement('img');
 img.src = pathArrayHeaven[digitIndexHour];
 
-const diveHeavenHour = document.getElementById('div-heaven-hour');
-diveHeavenHour.innerHTML = '';
-diveHeavenHour.appendChild(img);
+const divHeavenHour = document.getElementById('div-heaven-hour');
+divHeavenHour.innerHTML = '';
+divHeavenHour.appendChild(img);
 
 // for Day cycles
 
@@ -34,14 +33,13 @@ const currentDate = new Date();
 const differentTimeDay = Math.floor(currentDate - startDate) / (24 * 60 * 60 * 1000) ;
 
 const digitIndexDay = Math.floor(differentTimeDay % 10); // days and 10 elements
-console.log(digitIndexDay);
 
 const imgDay = document.createElement('img');
 imgDay.src = pathArrayHeaven[digitIndexDay];
 
-const diveHeavenDay = document.getElementById('div-heaven-day');
-diveHeavenDay.innerHTML = '';
-diveHeavenDay.appendChild(imgDay);
+const divHeavenDay = document.getElementById('div-heaven-day');
+divHeavenDay.innerHTML = '';
+divHeavenDay.appendChild(imgDay);
 
 
 // for Month cycles
@@ -57,21 +55,25 @@ const startDateMonth = new Date('2023-09-01');
 const currentDateMonth = new Date();
 
 const differentMonth = Math.floor(currentDateMonth - startDateMonth) / (30 * 24 * 60 * 60 * 1000) ;
-console.log(differentMonth);
+
 const digitIndexMonth = Math.floor(differentMonth % 10); // month and 10 elements
 
 const imgMonth = document.createElement('img');
+
 imgMonth.src = pathArrayHeavenMonth[digitIndexMonth];
 
-const diveHeavenMonth = document.getElementById('div-heaven-month');
-diveHeavenMonth.innerHTML = '';
-diveHeavenMonth.appendChild(imgMonth);
+const divHeavenMonth = document.getElementById('div-heaven-month');
+
+divHeavenMonth.innerHTML = '';
+
+divHeavenMonth.appendChild(imgMonth);
 
 
 
 // for YEAR cycles
 const arrayHeavenYear = ["gui", "Dzja", "i", "bin", "din", "wu", "dzi", "gen", "sin", "ren"];
 const pathArrayHeavenYear = [];
+
 for(let item = 0; item < arrayHeavenYear.length; item++) {
     pathArrayHeavenYear.push(`../img/${arrayHeavenYear[item]}.svg`);
 }
@@ -88,6 +90,6 @@ const digitIndexYear = Math.floor(differentYear % 10); // year and 10 elements
 const imgYear = document.createElement('img');
 imgYear.src = pathArrayHeavenYear[digitIndexYear];
 
-const diveHeavenYear = document.getElementById('div-heaven-year');
-diveHeavenYear.innerHTML = '';
-diveHeavenYear.appendChild(imgYear);
+const divHeavenYear = document.getElementById('div-heaven-year');
+divHeavenYear.innerHTML = '';
+divHeavenYear.appendChild(imgYear);
