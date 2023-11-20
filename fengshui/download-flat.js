40473db7d4b844c9f0d7eeb79b3ee6e620dae702
Frontenddevkan <frontenddevkan@gmail.com>
2023-11-20 22:33:14 +0500

    feat: add animations to main page

---
+++ fengshui/download-flat.js
@@ -0,0 +1,43 @@
+'use-strict';
+
+
+
+// получение ссылки на необходимые элементы страницы
+
+const uploadButton = document.getElementById("upload-button");
+const imageContainer = document.getElementById("image-container");
+
+// добавление обработчика события для клика на кнопку
+
+uploadButton.addEventListener("click", function() {
+    // создание элемента инпут для загрузки изображения
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+
+    // назначаем обработчик события выбора файла
+    fileInput.addEventListener("change", function(event) {
+            const file = e.target.files[0];
+            // создаем объект файлридер для чтения файла
+            const reader = new FileReader();
+            reader.onload = function() {
+   // создание элемента изображения и установливаем атрибуты используя данные изображения
+   const imageElement = document.createElement("img");
+    imageElement.src = reader.result;
+    imageElement.alt = file.name;
+
+    // очищаем контейнер от предыдущего изображения и вставляем новое
+
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(imageElement);
+}
+    // читаем содержимое выбранного файла в формате Дата урл
+    reader.readAsDataURL(file);
+ });
+
+    // кликаем на input элемент чтобы открыть диалоговое окно выбора файла
+
+        fileInput.click();
+    });
+
+
+
